37a52b04b5707c89ae6f8e5d0ae2e0e6e662626a
Musa Karakas <musakarakas@gmail.com>
2013-07-17 19:37:04 +0300

    Grouped more functions.
    Grouped all internationalization code into init_i18n().
    Grouped start-game end-game related code into Game.state object.
    Grouped level related code into Game.level object.

--- script.js
+++ script.js
@@ -4,28 +4,12 @@ var Game = {
     Game.init_timer();
     Game.init_stats();
     Game.init_tiles();
-    Game.init_actions();
-    Game.internationalize();
+    Game.init_state();
     View.repaint();
   },
-  internationalize: function(lng) {
-    var options = {
-      lng: lng || 'en',
-      fallbackLng: 'en',
-      postProcess: 'sprintf'
-    };
-    i18n.init(options, function() {
-      $('body').i18n();
-    });
-  },
-  reset: function() {
-    Game.reset_stats();
-    Game.reset_tiles();
-    Game.timer.start();
-  },
   init_options: function() {
-    Game.level = 4;
-    Game.language = 'en';
+    Game.init_levels();
+    Game.init_i18n();
   },
   init_stats: function() {
     Game.init_clicks();
@@ -35,20 +19,14 @@ var Game = {
     Game.clicks.reset();
     Game.timer.reset();
     Game.matches_found = 0;
-    Game.is_over = true;
   },
   init_tiles: function() {
     Game.tiles = [];
-    Game.tile_count = Math.pow(Game.level, 2) - (Game.level % 2);
-    Game.timer.set_max_time(Game.tile_count * 4);
-    for (var i = 0; i < Game.tile_count; i++)
-      Game.tiles[i] = new Tile(i % (Game.tile_count / 2));
+    var n = Game.level.tile_count();
+    Game.timer.set_max_time(n * 4);
+    for (var i = 0; i < n; i++)
+      Game.tiles[i] = new Tile(i % (n / 2));
     View.draw_tiles();
-  },
-  next_level: function() {
-    Game.level = (Game.level - 2) % 6 + 3; // 4 -> 5 -> 6 -> 7 -> 8 -> 3 -> 4
-    Game.init_tiles();
-    Game.reset_stats();
   },
   reset_tiles: function() {
     for (var i = 0; i < Game.tiles.length; i++)
@@ -56,9 +34,49 @@ var Game = {
     Game.shuffle_tiles();
     Game.repaint_tiles();
   },
+  init_levels: function() {
+    var level = 4;
+    $('#level-up').click(next);
+
+    Game.level = {tile_count: tile_count,
+      display: display, reset: reset, next: next, level: get};
+
+    function reset() {
+      level = 4;
+    }
+    function tile_count() {
+      return Math.pow(level, 2) - (level % 2);
+    }
+    function next() {
+      level = (level - 2) % 6 + 3; // 4 -> 5 -> 6 -> 7 -> 8 -> 3 -> 4
+      Game.init_tiles();
+      Game.reset_stats();
+      View.repaint();
+    }
+    function get() {
+      return level;
+    }
+    function display() {
+      var $btn = $('#level-up');
+      Game.state.over() ? Utils.enable_button($btn) : Utils.disable_button($btn);
+      $('#level').text(level + ' x ' + level);
+      var klass = level > 5 ? 'icon-th' : 'icon-th-large';
+      $('#level-up i').removeClass().addClass(klass);
+    }
+  },
+  init_i18n: function() {
+    set_language();
+    $('#language').click(set_language);
+    function set_language() {
+      var lng = i18n.lng() === 'en' ? 'tr' : 'en';
+      var options = {lng: lng, fallbackLng: 'en', postProcess: 'sprintf'};
+      i18n.init(options, function() {
+        $('body').i18n();
+      });
+    }
+  },
   init_timer: function() {
     var interval = null, time = 0, max_time = 0;
-    reset();
     Game.timer = {start: start, stop: stop, reset: reset,
       time: get_time, set_max_time: set_max_time, display: display};
 
@@ -68,7 +86,7 @@ var Game = {
         time++;
         display();
         if (time >= max_time)
-          Game.end_game(false);
+          Game.state.end(false);
       }, 1000);
     }
     function stop() {
@@ -93,6 +111,9 @@ var Game = {
     reset();
     Game.clicks = {reset: reset, increment: increment, value: get, is_odd: odd};
 
+    function get() {
+      return count;
+    }
     function set(n) {
       count = n;
       display();
@@ -103,9 +124,6 @@ var Game = {
     function increment() {
       set(count + 1);
     }
-    function get() {
-      return count;
-    }
     function odd() {
       return count % 2 === 1;
     }
@@ -113,24 +131,42 @@ var Game = {
       $('#clicks').text(count);
     }
   },
-  init_actions: function() {
+  init_state: function() {
+    var over = true;
+    Game.state = {end: end_game, over: is_over, display: display};
+
     $('#start-game').click(function() {
-      Game.start_game();
+      Game.reset_stats();
+      Game.reset_tiles();
+      Game.timer.start();
+      over = false;
+      $('#window').removeClass('overlay');
+      View.repaint();
     });
     $('#end-game').click(function() {
       if (!Game.matches_found || window.confirm(i18n.t('are-you-sure')))
-        Game.end_game();
-    });
-    $('#level-up').click(function() {
-      Game.next_level();
-      View.repaint();
-    });
-    $('#language').click(function() {
-      Game.internationalize(i18n.lng() === 'tr' ? 'en' : 'tr');
+        end_game();
     });
     $(window).resize(function() {
       View.resize();
     });
+    function end_game(win) {
+      Game.timer.stop();
+      Game.disable_tiles();
+      over = true;
+      if (arguments.length) {
+        var params = {sprintf: [Game.clicks.value(), Game.timer.time()]};
+        alert(i18n.t(win ? 'you-win' : 'you-lose', params));
+      }
+      View.repaint();
+    }
+    function is_over() {
+      return over;
+    }
+    function display() {
+      $('#start-game').toggleClass('invisible', !over);
+      $('#end-game').toggleClass('invisible', over);
+    }
   },
   shuffle_tiles: function() {
     for (var i = 0; i < Game.tiles.length; i++)
@@ -142,24 +178,7 @@ var Game = {
     }
   },
   all_matches_found: function() {
-    return Game.matches_found === Game.tile_count / 2;
-  },
-  start_game: function() {
-    Game.reset();
-    Game.is_over = false;
-    View.start_game();
-  },
-  end_game: function(win) {
-    Game.timer.stop();
-    Game.disable_tiles();
-    Game.is_over = true;
-    if (arguments.length)
-      show_message();
-    View.end_game();
-    function show_message() {
-      var params = {sprintf: [Game.clicks.value(), Game.timer.time()]};
-      alert(i18n.t(win ? 'you-win' : 'you-lose', params));
-    }
+    return Game.matches_found === Game.level.tile_count() / 2;
   },
   click_tile: function(tile) {
     Game.clicks.increment();
@@ -180,7 +199,7 @@ var Game = {
       Game.matches_found++;
       Game.tile_to_match.matched = tile.matched = true;
       if (Game.all_matches_found())
-        Game.end_game(true);
+        Game.state.end(true);
     }
     function hide(first_tile, second_tile) {
       setTimeout(function() {
@@ -200,7 +219,7 @@ var Game = {
       else
         Game.tiles[i].hide();
     }
-    if (Game.is_over)
+    if (Game.state.over())
       Game.disable_tiles();
   }
 };
@@ -210,7 +229,7 @@ var View = {
     $('#tiles').empty();
     for (var i = 0; i < Game.tiles.length; i++)
       Game.tiles[i].$button.appendTo($('#tiles'));
-    var tile_size = (100 / Game.level) + '%';
+    var tile_size = (100 / Game.level.level()) + '%';
     $('#tiles button').css({'width': tile_size, 'height': tile_size});
   },
   repaint: function() {
@@ -220,8 +239,8 @@ var View = {
   },
   repaint_stats: function() {
     Game.timer.display();
-    View.set_level();
-    View.set_start();
+    Game.level.display();
+    Game.state.display();
   },
   resize: function() {
     var w = $(window).width(), h = $(window).height(), size;
@@ -250,25 +269,6 @@ var View = {
       $('body').css('font-size', font_size);
       $('body').css('line-height', $('body').css('font-size'));
     }
-  },
-  set_level: function() {
-    var $button = $('#level-up');
-    Game.is_over ? Utils.enable_button($button) : Utils.disable_button($button);
-    $('#level').text(Game.level + ' x ' + Game.level);
-    var klass = Game.level > 5 ? 'icon-th' : 'icon-th-large';
-    $('#level-up i').removeClass().addClass(klass);
-  },
-  set_start: function() {
-    $('#start-game').toggleClass('invisible', !Game.is_over);
-    $('#end-game').toggleClass('invisible', Game.is_over);
-  },
-  start_game: function() {
-    $('#window').removeClass('overlay');
-    View.repaint();
-  },
-  end_game: function() {
-    $('#window').toggleClass('overlay', Game.matches_found === 0);
-    View.repaint();
   }
 };
 
